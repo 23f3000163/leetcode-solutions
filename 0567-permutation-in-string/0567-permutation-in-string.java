4ddc94df802b872d5284aba24dc6b56c10019876
Ashish Vishwakarma <23f3000163@ds.study.iitm.ac.in>
2026-08-24 16:27:09 +0530

    Time: 26 ms (25.79%), Space: 47 MB (11.02%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+
+        if (n > m) {
+            return false;
+        }
+
+
+        HashMap <Character, Integer> freq = new HashMap<>();
+        
+        for (int i = 0; i < n; i++) {
+            char ch = s1.charAt(i);
+            freq.put(ch, freq.getOrDefault(ch, 0) + 1);
+        }
+
+        int i = 0;
+        int windowlength = n;
+        int j = 0;
+
+        HashMap<Character, Integer> window = new HashMap<>();
+
+        for (j = 0 ; j < m; j++) {
+            char ch = s2.charAt(j);
+            window.put(ch, window.getOrDefault(ch, 0) + 1);
+
+            if ( j - i + 1 > n) {
+                window.put(s2.charAt(i), window.get(s2.charAt(i)) - 1);
+
+                if(window.get(s2.charAt(i)) == 0) {
+                    window.remove(s2.charAt(i));
+                }
+                i++;
+            }
+            
+            if (j - i + 1 ==n) {
+
+                if (window.equals(freq)) {
+                    return true;
+                }
+            }
+        } 
+        return false;
+    }
+}
