654e341caab154b657084f5516b9e7812cf6bc9a
Ashish Vishwakarma <23f3000163@ds.study.iitm.ac.in>
2026-08-27 22:45:04 +0530

    Time: 1 ms (99.94%), Space: 44.7 MB (99.7%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int compress(char[] chars) {
+
+        int readIndex = 0;
+        int writeIndex = 0;
+
+        while (readIndex < chars.length) {
+
+            char currentChar = chars[readIndex];
+            int count = 0;
+
+            // Count duplicate character
+            while (readIndex < chars.length &&
+                   currentChar == chars[readIndex]) {
+                readIndex++;
+                count++;
+            }
+
+            // Insert current character
+            // and make it available
+            chars[writeIndex] = currentChar;
+            writeIndex++;
+
+            // Insert the count
+            if (count > 1) {
+
+                String countStr = String.valueOf(count);
+
+                for (char digit : countStr.toCharArray()) {
+                    chars[writeIndex] = digit;
+                    writeIndex++;
+                }
+            }
+        }
+
+        return writeIndex;
+    }
+}
