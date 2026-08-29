c7e66f83b31b13390b068f8824fc86e58572eaaf
Ashish Vishwakarma <23f3000163@ds.study.iitm.ac.in>
2026-08-29 16:19:38 +0530

    Time: 9 ms (42.81%), Space: 41.9 MB (85.58%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,15 +1,10 @@
 class Solution {
     public int fib(int n) {
-        
-        if (n == 0) {
-            return 0;
+           // base condition
+        if (n < 2) {
+            return n;
         }
-
-        if (n == 1) {
-            return 1;
-        }
-
-        int ans = fib(n - 1) + fib(n - 2);
-        return ans;
+        //
+        return fib(n - 1) + fib(n - 2);
     }
 }
