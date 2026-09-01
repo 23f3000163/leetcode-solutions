dbaa18b097a6bf81c7fba03f4c6f585c9c814ccd
Ashish Vishwakarma <23f3000163@ds.study.iitm.ac.in>
2026-09-01 20:39:27 +0530

    Time: 0 ms (100%), Space: 48.4 MB (50.12%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,20 +1,29 @@
 class Solution {
+    public int solve(int[] nums, int target, int start, int end) {
+        
+        if(start > end) {
+            return -1;
+        }
+        
+        int mid = start + (end - start) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        } else if (target > nums[mid]) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+
+        return solve(nums, target, start, end);
+
+            
+    }
     public int search(int[] nums, int target) {
         int n = nums.length;
         int start = 0;
         int end = n - 1;
 
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            if (nums[mid] == target) {
-                return mid;
-            } else if (target > nums[mid]) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
-        }
-        return -1;
+        int ans = solve(nums, target, start, end);
+        return ans;
     }
 }
