0c831a832142f0fe62d8a1e24a383e5fccd235c5
Ashish Vishwakarma <23f3000163@ds.study.iitm.ac.in>
2026-09-21 21:44:59 +0530

    Time: 0 ms (100%), Space: 44.4 MB (35.43%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -3,13 +3,10 @@ class Solution {
         int n = nums.length;
         int[] ans = new int[n];
         
-        for (int i = 0; i < n; i++) {
-            if (i == 0) {
-                ans[0] = nums[0];
-            }
-            else {
-                ans[i] = ans[i - 1] + nums[i];
-            }   
+        ans[0] = nums[0];
+        
+        for (int i = 1; i < n; i++) {
+            ans[i] = ans[i - 1] + nums[i];  
         }
         return ans;
     }
