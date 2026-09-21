479d47276183acf4f5c1f6a5ed5b4ea0394c9dc6
Ashish Vishwakarma <23f3000163@ds.study.iitm.ac.in>
2026-09-21 19:08:39 +0530

    Time: 2 ms (81.98%), Space: 46.7 MB (28.16%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,71 @@
+class Solution {
+
+    static boolean isSafeToPlace(int colIndex, int rowIndex, char[][] chessboard , int n) {
+        int row = rowIndex;
+        int col = colIndex;
+
+        while (col >= 0) {
+            if(chessboard[row][col] == 'Q') {
+                return false;
+            }
+            col--;
+        }
+
+        row = rowIndex;
+        col = colIndex;
+
+        while (col >= 0 && row >= 0) {
+            if(chessboard[row][col] == 'Q') {
+                return false;
+            }
+            row = row - 1;
+            col = col - 1;
+        }
+
+        row = rowIndex;
+        col = colIndex;
+
+        while (col >= 0 && row < n) {
+            if(chessboard[row][col] == 'Q') {
+                return false;
+            }
+            row = row + 1;
+            col = col - 1;
+        }
+        return true;
+    }
+
+    static void solve(int n, char[][] chessboard, int colIndex, List<List<String>> ans){
+        if(colIndex >= n) {
+            List<String> temp = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                temp.add(new String(chessboard[i]));
+            }
+            ans.add(temp);
+            return;
+        }
+        
+        for (int rowIndex = 0; rowIndex < n; rowIndex++) {
+            if(isSafeToPlace(colIndex, rowIndex, chessboard, n)) {
+                chessboard[rowIndex][colIndex] = 'Q';
+                solve(n, chessboard, colIndex + 1, ans);
+                //BACKTRACK
+                chessboard[rowIndex][colIndex] = '.';
+            }
+        }
+    }
+
+    public List<List<String>> solveNQueens(int n) {
+        
+        char[][] chessboard = new char[n][n];
+        for (int i = 0; i < n; i ++) {
+            Arrays.fill(chessboard[i], '.');
+        }
+
+        int colIndex = 0;
+        List<List<String>> ans = new ArrayList<>();
+       
+        solve(n, chessboard, colIndex, ans);
+        return ans;
+    }
+}
