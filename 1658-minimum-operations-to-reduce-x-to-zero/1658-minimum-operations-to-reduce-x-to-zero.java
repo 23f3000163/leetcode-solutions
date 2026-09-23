5fd4d1215ff1aa1f6a95f811ebcc18214c7efb43
Ashish Vishwakarma <23f3000163@ds.study.iitm.ac.in>
2026-09-24 02:14:33 +0530

    Time: 95 ms (5.08%), Space: 166.1 MB (5.01%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
@@ -4,20 +4,20 @@ class Solution {
         HashMap<Integer, Integer> freq = new HashMap<>();
         freq.put(0, -1);
 
-        int totalSum = 0;
+        int sum = 0;
         for(int i = 0; i < n; i++) {
-            totalSum += nums[i];
-            freq.put(totalSum, i);
+            sum += nums[i];
+            freq.put(sum, i);
 
         }
 
-        if (totalSum < x) {
+        if (sum < x) {
             return - 1;
         }
 
-        int target = totalSum - x;
+        int target = sum - x;
         int longest = Integer.MIN_VALUE;
-        int sum = 0;
+        sum = 0;
 
         for(int i = 0; i < n; i++) {
             sum += nums[i];
